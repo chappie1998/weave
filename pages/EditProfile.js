a9ed89bf99fc70e260a39c315a5d88fce972eee0
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-09 16:59:09 +0530

    fixed

--- pages/EditProfile.js
+++ pages/EditProfile.js
@@ -8,7 +8,7 @@ const EditProfile = () => {
          <span className="px-3"> Lenster Cover Photo👋</span> 
         </div>
       
-      <div class="w-full max-w-xs">
+      <div className="w-full max-w-xs">
       <div className="post__avatarppd p-2 border-double border-red-500">
         <img
           className="w-20 h-20 -mt-6  rounded-full cursor-pointer ring-8 ring-purple-400"
@@ -18,25 +18,25 @@ const EditProfile = () => {
           alt="Rounded avatarppd"
         />
       </div>
-        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div class="mb-4">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-700 text-sm font-bold mb-2"
               for="username"
             >
               Update BIO
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
               placeholder="Username"
             />
           </div>
 
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <button
-              class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
             >
               Submit
