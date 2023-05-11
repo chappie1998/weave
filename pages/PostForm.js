b885909fee8adb1ffa87232fcd092a827f5104a3
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-11 21:34:00 +0530

    Post JSON will be created

--- pages/PostForm.js
+++ pages/PostForm.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const PostForm = () => {
+  const [formData, setFormData] = useState({});
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const jsonData = JSON.stringify(formData);
+    console.log(jsonData);
+    setFormData({})
+  };
+
+  const handlechange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+
+  };
   return (
-    <div className='bg-black h-96 p-9 rounded-2xl'>
-     <div className="post__avatarppd p-2 border-double border-red-500">
+    <div className="bg-black h-96 p-9 rounded-2xl">
+      <div className="post__avatarppd p-2 border-double border-red-500">
         <img
           className="w-10 h-10  rounded-full cursor-pointer ring-8 ring-purple-400"
           src={
@@ -11,12 +27,29 @@ const PostForm = () => {
           }
           alt="Rounded avatarppd"
         />
-        </div>
-    <label htmlFor="message" className="block mb-2 font-medium text-gray-900 text-xl ">Whats on Your mind</label>
-    <textarea id="message" rows="4" className="block p-2.5 w-full text-xl text-white bg-black rounded-lg     " placeholder="Leave a comment..."></textarea>
-    <button className="button m-4 px-10 py-3 rounded-full bg-purple-500 text-white items-center justify-center">Post</button>
+      </div>
+      <label
+        htmlFor="message"
+        className="block mb-2 font-medium text-white text-xl "
+      >
+        Whats on Your mind
+      </label>
+      <form onSubmit={handleSumbit}>
+        <textarea
+          id="message"
+          name="text"
+          value={formData.text || ""}
+          onChange={handlechange}
+          rows="4"
+          className="block p-2.5 w-full text-xl text-white bg-black rounded-lg     "
+          placeholder="Leave a comment..."
+        ></textarea>
+        <button className="button m-4 px-10 py-3 rounded-full bg-purple-500 text-white items-center justify-center">
+          Post
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default PostForm
+export default PostForm;
