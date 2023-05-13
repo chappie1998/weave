f326e3f54f2fd0fadeb47c48e1f1a00e418db629
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-13 10:35:02 +0530

    Check:Api calling issue

--- pages/Right.js
+++ pages/Right.js
@@ -1,155 +1,45 @@
-import React, { useState,useEffect } from "react";
-import { AiOutlineHeart } from "react-icons/ai";
-import { BiChat } from "react-icons/bi";
-import { BsArrowDownUp } from "react-icons/bs";
-import { useRouter } from "next/router";
-import { ToastContainer, toast } from "react-toastify";
+import React, { useState, useEffect } from "react";
+
 import "react-toastify/dist/ReactToastify.css";
 
-import PPD from "./ProfileDropDown";
-import Link from "next/link";
-
-
-
+import Post from "./Post";
 
 const Right = () => {
-  const [timestamps, setTimestamps] = useState("")
-  const [avtars, setAvtars] = useState("")
-
+  const [profiledetails, setProfiledetails] = useState([]);
 
   useEffect(() => {
-    PostDetails();
+    fetchProfileDetails();
   }, []);
-  
-  
-  
-  const PostDetails = async () => {
+
+  const fetchProfileDetails = async () => {
     try {
-      const data = await fetch("https://peerpost-api.vercel.app/post/details/0");
-      const result = await data.json();
-      
-      setTimestamps(Math.floor((Date.now() - (result.post.timetamp) * 1000) / (1000 * 60 * 60)))
-      setAvtars(result.post.token_uri)
+      const response = await fetch(
+        "https://peerpost-api.vercel.app/profile/details/1"
+      );
+      const data = await response.json();
+      setProfiledetails(data);
+      console.log(data);
     } catch (error) {
       console.error(error);
     }
   };
-  const Post = ({ displayName, userName, text, image, avatar, timestamp }) => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const notify = () => toast.error("Please Sign In Your Wallet");
-    const router = useRouter();
-    const handleCommentClick = () => {
-      router.push({
-        pathname: "/CommentsPage",
-       
-        state: {
-          post: { displayName, userName, text, image, avatar, timestamp },
-        },
-      });
-    };
-  
- 
-    
-
-    return (
-      <>
-        <div className="flex flex-row container items-center justify-center bg-white ">
-          <div className="post flex flex-row border rounded-t-lg shadow px-8">
-            <div className="flex flex-col ">
-              <div className="">
-                <div
-                  className="dropdown absolute"
-                  onMouseEnter={() => setIsDropdownOpen(true)}
-                  onMouseLeave={() => setIsDropdownOpen(false)}
-                >
-                  <Link href={"/Profile"}>
-                    <button className="dropdown-button  -translate-x-5 translate-y-5">
-                      {" "}
-                      <img
-                        className="w-10 h-10 border bg-gray-600   rounded-full cursor-pointer    "
-                        src={avatar}
-                        alt="Rounded avatar"
-                      />
-                    </button>
-                  </Link>
-                  {isDropdownOpen && <PPD />}
-                </div>
-              </div>
-            </div>
-
-            <div className="post__body w-full my-4 mx-6">
-              <div className="post__header">
-                <div className="post__headerText">
-                  <h3 className="font-medium flex flex-col">
-                    {displayName}
-                    <span className="post__headerSpecial font-thin text-sm text-red-400">
-                      @{userName}{" "}
-                      <span className="text-black">- {timestamp}</span>
-                    </span>
-                  </h3>
-                </div>
-                <div className="post__headerDescription break-words space-y-6">
-                  <p>{text}</p>
-                </div>
-              </div>
-              <img src={image} alt="" className="p-10 h-80 w-96 " />
-              <div className="post__footer flex flex-row space-x-6">
-            <button
-              onClick={handleCommentClick}
-              className="hover:bg-blue-200 rounded-full p-1.5 "
-            >
-              {" "}
-              <BiChat
-              className=" h-5 w-10" />{" "}
-            </button>{" "}
-            <div className="hover:bg-red-200 rounded-full p-1.5">
-              {" "}
-              <AiOutlineHeart
-                onClick={notify}
-                className="h-5 w-10"
-              />
-              <ToastContainer
-                position="bottom-right"
-                autoClose={1000}
-                hideProgressBar={true}
-              />
-            </div>
-            <div className=" hover:bg-purple-200 rounded-full p-1.5">
-              {" "}
-              <BsArrowDownUp
-                onClick={notify}
-                className="h-5 w-10"
-              />
-              <ToastContainer
-                position="bottom-right"
-                autoClose={1000}
-                hideProgressBar={true}
-              />
-            </div>
-          </div>
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  };
 
   return (
-    <>
-      <div className="flex flex-row container mx-auto max-w-screen-xl">
-        <div>
+    <div className="flex flex-row container mx-auto max-w-screen-xl">
+      <div>
+        {[profiledetails].map((profile) => (
           <Post
+            key="{profile.profile.profile_id}"
             displayName="Mr X"
-            userName="MR X"
-            text={avtars}
-            avatar={avtars}
-            image={avtars}
-            timestamp={timestamps+"h"}
+            userName="{profile.handle}"
+            text={profile.posts[0].data.content}
+            avatar="{profile.token_uri}"
+            image="{avtars}"
+            timestamp=""
           />
-        
-        </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
