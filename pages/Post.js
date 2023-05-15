ef55f8229b340329fa6cf4ca8bc354195e0c8b75
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-15 13:01:43 +0530

    Css fixed

--- pages/Post.js
+++ pages/Post.js
@@ -4,14 +4,11 @@ import { BiChat } from "react-icons/bi";
 import { BsArrowDownUp } from "react-icons/bs";
 import PPD from "./ProfileDropDown";
 import Link from "next/link";
-import NextLink from "next/link";
-
 import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const Post = ({
-  id,
   postID,
   displayName,
   userName,
@@ -27,93 +24,103 @@ const Post = ({
   const handleCommentClick = () => {
     router.push({
       pathname: "/CommentsPage",
-      query: { key },
+      query: { postID },
       state: {
-        post: {postID,displayName, userName, text, images, avatar, timestamp },
+        post: {
+          postID,
+          displayName,
+          userName,
+          text,
+          images,
+          avatar,
+          timestamp,
+        },
       },
     });
   };
 
   return (
-    <div className=" container flex flex-row items-center justify-center   ">
-     <Link href={`/posts/${postID}`}><div className="post flex flex-row border border-solid shadow-sm py-5 px-7 hover:bg-slate-100 cursor-pointer  sm:w-4/5  ">
-        <div className="flex flex-col ">
-          <div className="">
-            <div
-              className="dropdown absolute"
-              onMouseEnter={() => setIsDropdownOpen(true)}
-              onMouseLeave={() => setIsDropdownOpen(false)}
-            >
-              <Link href={`/u/${userName}`}>
-                <button className="dropdown-button  transition-transform   -translate-x-5 translate-y-5">
-                  {" "}
-                  <img
-                    className="w-10 h-10 border bg-gray-600  rounded-full cursor-pointer    "
-                    src={avatar}
-                    alt="Rounded avatar"
-                  />
-                </button>
-              </Link>
-              {isDropdownOpen && <PPD profileData={profileData} />}
+    <div className=" container flex flex-row items-center justify-center  ">
+      <Link href={`/posts/${postID}`}>
+        <div className="post flex flex-row border border-solid shadow-sm py-5 px-7 hover:bg-slate-100 cursor-pointer  sm:w-4/5  ">
+          <div className="flex flex-col ">
+            <div className="">
+              <div
+                className="dropdown absolute"
+                onMouseEnter={() => setIsDropdownOpen(true)}
+                onMouseLeave={() => setIsDropdownOpen(false)}
+              >
+                <Link href={`/u/${userName}`}>
+                  <button className="dropdown-button  transition-transform -mt-4   -translate-x-5 translate-y-5">
+                    {" "}
+                    <img
+                      className="w-10 h-10 border bg-gray-600  rounded-full cursor-pointer    "
+                      src={avatar}
+                      alt="Rounded avatar"
+                    />
+                  </button>
+                </Link>
+                {isDropdownOpen && <PPD profileData={profileData} />}
+              </div>
+            </div>
+          </div>
+
+          <div className="post__body w-full mx-6">
+            <div className="post__header">
+              <div className="post__headerText">
+                <Link href={`/u/${userName}`}>
+                  <h3 className="font-medium flex flex-col">
+                    {userName}
+
+                    <span className="post__headerSpecial font-thin text-sm text-red-400">
+                      @{userName}
+                      <span className="text-black">- {timestamp}</span>
+                    </span>
+                  </h3>
+                </Link>
+              </div>
+              <div className="post__headerDescription break-words my-5 space-y-6">
+                <p>{text}</p>
+              </div>
+            </div>
+            {images.map((image) => (
+              <img
+                key={Math.random()}
+                src={image}
+                alt=""
+                className="p-10 h-auto w-auto "
+              />
+            ))}
+            <div className="post__footer flex flex-row space-x-6">
+              <button
+                onClick={handleCommentClick}
+                className="hover:bg-blue-200 rounded-full px-1.5  "
+              >
+                {" "}
+                <BiChat className=" h-5 w-10" />{" "}
+              </button>{" "}
+              <div className="hover:bg-red-200 rounded-full p-1.5">
+                {" "}
+                <AiOutlineHeart onClick={notify} className="h-5 w-10" />
+                <ToastContainer
+                  position="bottom-right"
+                  autoClose={1000}
+                  hideProgressBar={true}
+                />
+              </div>
+              <div className=" hover:bg-purple-200 rounded-full p-1.5">
+                {" "}
+                <BsArrowDownUp onClick={notify} className="h-5 w-10" />
+                <ToastContainer
+                  position="bottom-right"
+                  autoClose={1000}
+                  hideProgressBar={true}
+                />
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="post__body w-full mx-6">
-          <div className="post__header">
-            <div className="post__headerText">
-              <Link href={`/u/${userName}`}>
-                <h3 className="font-medium flex flex-col">
-                  {userName}
-
-                  <span className="post__headerSpecial font-thin text-sm text-red-400">
-                    @{userName}
-                    <span className="text-black">- {timestamp}</span>
-                  </span>
-                </h3>
-              </Link>
-            </div>
-            <div className="post__headerDescription break-words my-5 space-y-6">
-              <p>{text}</p>
-            </div>
-          </div>
-          {images.map((image) => (
-            <img
-              key={Math.random()}
-              src={image}
-              alt=""
-              className="p-10 h-auto w-auto "
-            />
-          ))}
-          <div className="post__footer flex flex-row space-x-6">
-            <button
-              onClick={handleCommentClick}
-              className="hover:bg-blue-200 rounded-full px-1.5  "
-            >
-              {" "}
-              <BiChat className=" h-5 w-10" />{" "}
-            </button>{" "}
-            <div className="hover:bg-red-200 rounded-full p-1.5">
-              {" "}
-              <AiOutlineHeart onClick={notify} className="h-5 w-10" />
-              <ToastContainer
-                position="bottom-right"
-                autoClose={1000}
-                hideProgressBar={true}
-              />
-            </div>
-            <div className=" hover:bg-purple-200 rounded-full p-1.5">
-              {" "}
-              <BsArrowDownUp onClick={notify} className="h-5 w-10" />
-              <ToastContainer
-                position="bottom-right"
-                autoClose={1000}
-                hideProgressBar={true}
-              />
-            </div>
-          </div>
-        </div>
-      </div></Link> 
+      </Link>
     </div>
   );
 };
