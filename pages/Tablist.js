9d2689266c15479dded45b337d14791fe705a472
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-17 10:54:35 +0530

    Now on select bottom reamins blue

--- pages/Tablist.js
+++ pages/Tablist.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
-import Link from "next/link";
 
 const Tablist = ({ handleTablistSelect }) => {
   const [activeTab, setActiveTab] = useState("Popular");
+
+  const handleTabClick = (text) => {
+    setActiveTab(text);
+    handleTablistSelect(text);
+  };
 
   return (
     <div className="px-8 mx-7 text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
@@ -15,16 +19,12 @@ const Tablist = ({ handleTablistSelect }) => {
         ].map(({ text }) => (
           <li className="mr-2" key={text}>
             <p
-              className={`inline-block p-4 border-b-4 border-transparent cursor-pointer rounded-t-lg hover:text-gray-600 hover:border-blue-500 font-bold ${
+              className={`inline-block p-4 border-b-4 cursor-pointer rounded-t-lg hover:text-gray-600 hover:border-blue-500 font-bold ${
                 activeTab === text
-                  ? "text-blue-600 border-blue-500  active "
-                  : ""
+                  ? "text-blue-600 border-blue-500"
+                  : "text-gray-500 border-transparent"
               }`}
-              aria-current={activeTab === text ? "page" : undefined}
-              onClick={() => {
-                setActiveTab(text);
-                handleTablistSelect(text);
-              }}
+              onClick={() => handleTabClick(text)}
             >
               {text}
             </p>
