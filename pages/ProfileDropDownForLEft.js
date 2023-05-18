4ef4fb64542eafe51c92f076c425f1da1eb0f5a7
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-05-18 15:59:31 +0530

    Follow Button added

--- pages/ProfileDropDownForLEft.js
+++ pages/ProfileDropDownForLEft.js
@@ -61,7 +61,7 @@ const ProfileDropdownForLeft = ({
             <li>Github</li>
           </ul>
         </div>
-        <div class="border border-gray-300 w-72 mb-2 "></div>
+        <div class="border border-gray-300 w-72 mb-2 mt-2 "></div>
         <div>
           <button
             onClick={() => {
